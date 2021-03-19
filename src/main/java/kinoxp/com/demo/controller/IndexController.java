defa9fe2e4720eba8aced1b1c625ca1b30d30e92
peter <peter.lm.schmidt@gmail.com>
2021-03-19 10:12:14 +0100

    test FilterShowsByDate tilføjet

--- src/main/java/kinoxp/com/demo/controller/IndexController.java
+++ src/main/java/kinoxp/com/demo/controller/IndexController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.request.WebRequest;
 
-import java.util.Date;
-
 @CrossOrigin(value = "*")
 @Controller
 public class IndexController {
--- src/test/java/kinoxp/com/demo/service/FilterShowsByDateTest.java
+++ src/test/java/kinoxp/com/demo/service/FilterShowsByDateTest.java
@@ -1,9 +1,9 @@
 package kinoxp.com.demo.service;
 
 import kinoxp.com.demo.model.ShowsEntity;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.sql.Date;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,13 +11,28 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FilterShowsByDateTest {
 
-    @BeforeEach
-    void setUp() {
-        List<ShowsEntity> testList = new ArrayList<>();
-    }
-
     @Test
     void returnFilteredShowList() {
+        ShowsEntity show1 = new ShowsEntity();
+        ShowsEntity show2 = new ShowsEntity();
+        ShowsEntity show3 = new ShowsEntity();
+        show1.setDate(new Date(System.currentTimeMillis()));
+        show2.setDate(new Date((long) (System.currentTimeMillis()+8.64e+7*90)));
+        show3.setDate(new Date((long) (System.currentTimeMillis()+8.64e+7*92)));
 
+        List<ShowsEntity> testDataList = new ArrayList<>();
+        testDataList.add(show1);
+        testDataList.add(show2);
+        testDataList.add(show3);
+
+        List<ShowsEntity> resultList;
+        List<ShowsEntity> correctList = new ArrayList<>();
+        correctList.add(show1);
+        correctList.add(show2);
+
+        FilterShowsByDate filterClass = new FilterShowsByDate();
+        resultList = filterClass.returnFilteredShowList(testDataList);
+
+        assertEquals(correctList,resultList);
     }
 }
